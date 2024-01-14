c6f668f90d7b2d2fbd764759d07430b4ee5411a3
Agustin Henry Marine Monteiro <141757322+lianmarine@users.noreply.github.com>
2024-01-14 11:34:52 +0000

    fre

--- src/streamlit2.py
+++ src/streamlit2.py
@@ -18,7 +18,7 @@ def main():
     
     uploaded_file = st.file_uploader("Cargar imagen", type=["jpg", "jpeg", "png"])
     #Carga el modelo
-    model = load_model('/workspaces/LungsFPruebaStreamlit/models/modelo_softmax.keras')
+    model = load_model('models/modelo_softmax.keras')
 
     if uploaded_file is not None:
 
